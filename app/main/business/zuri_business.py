aa894fce9c44518960fb258bd5260d112f3d9b9c
trustedcoder <trustedcoder11@gmail.com>
2022-11-03 15:06:44 +0100

    added openai

--- app/main/business/zuri_business.py
+++ app/main/business/zuri_business.py
@@ -1,3 +1,7 @@
+import re
+import os
+import openai
+
 
 class AppBusiness:
     @staticmethod
@@ -6,11 +10,29 @@ class AppBusiness:
             result = data['x'] + data['y']
         elif data['operation_type'] == 'subtraction':
             result = data['x'] - data['y']
-        else:
+        elif data['operation_type'] == 'multiplication':
             result = data['x'] * data['y']
-        response = {
+        else:
+            openai.api_key = os.getenv("OPENAI_API_KEY")
+            gpt_prompt = "I want the answer to this question. "+str(data['operation_type'])
+            response = openai.Completion.create(
+                engine="text-davinci-002",
+                prompt=gpt_prompt,
+                temperature=0.5,
+                max_tokens=256,
+                top_p=1.0,
+                frequency_penalty=0.0,
+                presence_penalty=0.0
+            )
+            answer = re.findall(r'\d+', response['choices'][0]['text'])
+            if(len(answer) > 0):
+                result = int(answer[0])
+            else:
+                result = 0
+
+        response_obj = {
             'slackUsername': 'trustedcoder',
             'result': result,
             'operation_type': data['operation_type']
         }
-        return response, 200
+        return response_obj, 200
--- app/main/util/req_parser.py
+++ app/main/util/req_parser.py
@@ -2,6 +2,6 @@ from flask_restx import reqparse
 
 
 payload_data = reqparse.RequestParser()
-payload_data.add_argument("operation_type",type=str,required=True,location="json",help="operation type should be subtraction, addition or multiplication, ",choices=list(['subtraction','addition','multiplication']))
-payload_data.add_argument("x",type=int,required=True,location="json",help="x should be an integer")
-payload_data.add_argument("y",type=int,required=True,location="json",help="y should be an integer")
+payload_data.add_argument("operation_type",type=str,required=True,location="json",help="operation type should be subtraction, addition or multiplication, ")
+payload_data.add_argument("x",type=int,required=False,location="json",help="x should be an integer")
+payload_data.add_argument("y",type=int,required=False,location="json",help="y should be an integer")
